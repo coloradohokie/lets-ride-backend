94253e741645a2964ebf58983bb14eacd2ae4e41
Doug Johnson <douglas.j.johnson@gmail.com>
2020-02-12 09:39:17 -0700

    update rides to show also show rider

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,11 +1,11 @@
 class RidesController < ApplicationController
     def index
         @rides = Ride.all
-        render json:@rides, include: :route
+        render json:@rides, include: ['route', 'riders']
     end
 
     def show
         @ride = Ride.find(params[:id])
-        render json:@ride, include: :route
+        render json:@ride, include: ['route', 'riders']
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Rider.destroy_all
 Route.destroy_all
 Motorcycle.destroy_all
@@ -25,6 +17,7 @@ doug_moto1 = Motorcycle.create(make: "BMW", model: "RS 1200", year: "2017", ride
 
 ride1 = Ride.create(date_time: "6/1/2021", description: "Fun ride for everybody", route_id:route1.id)
 ride2 = Ride.create(date_time: "7/1/2021", description: "Intense ride", route_id:route2.id)
+ride3 = Ride.create(date_time: "1/1/2019", description: "Nice ride", route_id: route1.id)
 
 photo1 = Photo.create(image_path: "", ride_id: ride1.id)
 photo2 = Photo.create(image_path: "", ride_id: ride1.id)
