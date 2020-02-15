096e91639a4bf1dd2ed5e564294465e57130ec64
Michael Newman <coloradohokie@gmail.com>
2020-02-15 00:37:06 -0700

    improve lightbox

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -20,13 +20,12 @@ class RidesController < ApplicationController
     end
 
     def update
-        byebug
         ride = Ride.find(params[:id])
         ride.update(date_time: params[:date],
                     description: params[:description],
-                    route_id: params[:route_id]
+                    route_id: params[:route]
                     )
 
-        redirect_to("http://localhost:3001/rides/#{ride.id}")
+        redirect_to("http://localhost:3001/ride.html?id=#{ride.id}")
     end
 end
