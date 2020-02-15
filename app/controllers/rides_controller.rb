fb483b18ef3dcc3874a0017bea9480a77acac2a3
Michael Newman <coloradohokie@gmail.com>
2020-02-14 21:11:22 -0700

    updates

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -20,12 +20,13 @@ class RidesController < ApplicationController
     end
 
     def update
+        byebug
         ride = Ride.find(params[:id])
         ride.update(date_time: params[:date],
                     description: params[:description],
-                    route_id: params[:route]
+                    route_id: params[:route_id]
                     )
 
-        redirect_to("http://localhost:3000/ride/#{ride.id}")
+        redirect_to("http://localhost:3001/rides/#{ride.id}")
     end
 end
