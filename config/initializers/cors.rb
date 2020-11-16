ed39d98781cb47f8b2217f7229532c95d478ee53
Michael Newman <coloradohokie@gmail.com>
2020-11-16 16:33:00 -0700

    This is a message.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-    origins 'localhost:1234', 'localhost:3000'
+    origins '*'
 
     resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
 end
+
+##############
