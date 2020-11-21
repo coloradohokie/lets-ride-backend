393d1559fc44fec4dd69e1705e888a7820fd247d
Michael Newman <coloradohokie@gmail.com>
2020-11-21 16:41:35 -0700

    changing avatar to see if I can get it working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    mount_uploader :avatar, AvatarUploader
+    # mount_uploader :avatar, AvatarUploader
     has_many :motorcycles
     has_many :ride_attendances
     has_many :rides, through: :ride_attendances
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -2,7 +2,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-  include Cloudinary::CarrierWave
+  
+  
+  # include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
