6364d1b9018f1f8a3e916dfc6d0f4c5e104ffd93
Michael Newman <coloradohokie@gmail.com>
2020-11-22 21:24:02 -0700

    add map_url to routes update method

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -28,6 +28,7 @@ class RoutesController < ApplicationController
         @route.start_location = params[:start_location] if params[:start_location]
         @route.end_location = params[:end_location] if params[:end_location]
         @route.description = params[:description] if params[:description]
+        @route.map_url = params[:map_url] if params[:map_url]
         @route.created_by = params[:created_by] if params[:created_by]
         if @route.save
             render json:@route, status: :ok
